a97b784b03dd6de99b3537739144615a4c9adf1b
Enrique Cruz <enrique.cs095@gmail.com>
2020-03-08 01:37:25 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,40 +1,71 @@
 #include "Bitmap.h"
 
 
-
-void Menu1(char** argv){
+bool Validacion1(char** argv){
+Bitmap bitmap;
 std::ifstream File;
 File.open(argv[2]);
-if(File.fail()){ std::cout<<"Archivo no existe\n"; return;}
-
-
-
-
+    if(File.fail()){ std::cout<<"Archivo \""<<argv[2]<<"\" no existe\n"; return false;}
+strcpy(bitmap.NameFile,argv[2]);
+bitmap.ReadMetadata();
+    if(strcmp(bitmap.Type,"BM")!=0 || bitmap.SizeP!=24){std::cout<<"Formato de archivo no soportado\n";
+    return false;}
+    if(bitmap.Reserved1>0){std::cout<<"Imagen ya contiene mensaje\n"; return false;}
+    return true;
 }
 
-void Menu2(char** argv){
-
-    
+bool Validacion2(char** argv){
+int c=0;
+   if(strcmp(argv[3],"-m")==0){c=1;}
+   if(strcmp(argv[3],"-f")==0){c=2;}
+   if(c==0){ std::cout<<"Commando \""<<argv[3]<<"\" no existe\n"; return false;}
+   return true;
 }
 
-void Menu3(char** argv){
-
-    
-}
-void Menu4(char** argv){
-
-    
-}
-
-
-bool FileExist(char * name){
+bool Validacion3(char** argv){
+Bitmap bitmap;
 std::ifstream File;
-File.open(name);
-if(File.fail()){
-    std::cout<<"Archivo no existe\n";
-    return false;
+File.open(argv[2]);
+    if(File.fail()){ std::cout<<"Archivo \""<<argv[2]<<"\" no existe\n"; return false;}
+strcpy(bitmap.NameFile,argv[2]);
+bitmap.ReadMetadata();
+    if(strcmp(bitmap.Type,"BM")!=0 || bitmap.SizeP!=24){std::cout<<"Formato de archivo no soportado\n";
+    return false;}
+    if(bitmap.Reserved1==0){
+        std::cout<<"Archivo \""<<argv[2]<<"\" no contiene mensaje\n";
+        return false;
+    }
+    return true;
 }
-return true;
+
+void IniciarComando1(char** argv){
+Bitmap bitmap;
+if(bitmap.SaveString(argv[4])){ std::cout<<"Datos guardados\n";};
+}
+
+void IniciarComando2(char** argv){
+Bitmap bitmap;
+std::ifstream File;
+std::string aux1;
+std::string aux2;
+File.open(argv[4]);
+    if(File.fail()){std::cout<<"Archivo \""<<argv[4]<<"\" no existe\n";return;}
+    while(!File.eof()){
+        getline(File,aux1);
+        aux2+=aux1;
+}
+    if(aux2.length()>0){
+        char* aux3=new char[aux2.length()+1];
+        aux3[aux2.length()]=0;
+        strcpy(aux3,aux2.c_str());
+        if(bitmap.SaveString(aux3)){std::cout<<"Datos guardados\n";};
+    }
+}
+
+void IniciarComando3(char** argv){
+Bitmap bitmap;
+strcpy(bitmap.NameFile,argv[2]);
+bitmap.ReadString();
 }
 
 void PrintError(){
@@ -53,41 +84,35 @@ int main(int argc, char**argv){
             if(strcmp(argv[1],"-t")==0){c=3;}
             if(strcmp(argv[1],"-v")==0){c=4;}
         switch(c){
-
             case 0:
             std::cout<<"Commando \""<<argv[1]<<"\" no existe\n";
             break;    
 
             case 1:
-                if(argc==2){
-                    PrintError();
-                    return 0;
+                if(argc==2){PrintError();return 0;}
+                if(!Validacion1(argv)){ return 0;}
+                if(argc==3){PrintError();return 0;}
+                if(!Validacion2(argv)){ return 0;}
+                if(argc==4){PrintError();return 0;}
+                if(strcmp(argv[3],"-m")==0){
+                    IniciarComando1(argv);
+                }else{
+                    IniciarComando2(argv);
                 }
-                Menu1(argv);
                 break;
 
             case 2:
-             if(argc==2){
-                    PrintError();
-                    return 0;
-                }
-                Menu2(argv);
+             if(argc==2){PrintError();return 0;}
+                 if(!Validacion3(argv)){return 0;
+                 }else{ IniciarComando3(argv);}
                 break;
 
             case 3:
-             if(argc==2){
-                    PrintError();
-                    return 0;
-                }
-                Menu3(argv);
+     
                 break;
 
             case 4: 
-                 if(argc==2){
-                    PrintError();
-                    return 0;
-                }
-                Menu4(argv);
+       
                 break;
         }
 
